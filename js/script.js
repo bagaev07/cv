4d8c15131e693a3bbe77a458913b52dc80e8edc0
[bagaev07] <[bagaev07@list.ru]>
2023-05-20 20:47:40 +0300

    final modification

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-const select = document.querySelectorAll(".section");
+const sections = document.querySelectorAll(".section");
 const sectBtns = document.querySelectorAll(".controls");
 const sectBtn = document.querySelectorAll(".control");
-const allSection = document.querySelector(".main-content");
+const allSections = document.querySelector(".main-content");
 
 function PageTransitions (){
     //Button click active class
@@ -12,6 +12,25 @@ function PageTransitions (){
             this.className += ' active-btn';
         })
     }
+    // Sections Active
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected from the other btns
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+
+            e.target.classList.add('active')
+
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 
